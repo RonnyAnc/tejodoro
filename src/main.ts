ba512d6d635b66ad219f882f48dd3927da24824b
Ronny Ancorini Rodríguez <ronnyancorini@gmail.com>
2022-03-21 20:16:24 +0100

    Log port when starting the application

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,7 @@ app.get('/api/hello', function (_, res) {
     res.send('Hello World');
 });
 
-
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log('App is running...')
+    console.log(`App is running in ${port}...`)
 });
