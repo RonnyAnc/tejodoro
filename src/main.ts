8679bde351829b28d87d4ac1120ff6ecd05ec744
Ronny Ancorini Rodríguez <ronnyancorini@gmail.com>
2022-03-21 20:04:46 +0100

    Fix port for heroku

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ app.get('/api/hello', function (_, res) {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
     console.log('App is running...')
 });
