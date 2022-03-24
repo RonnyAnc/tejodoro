718fcd73a54bb8f4ed1aaa57ac52e7d1bd9f3e6c
Fran Ortiz <fortizabril@gmail.com>
2022-03-24 09:11:22 +0100

    Add welcome page

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,15 @@ import express from 'express';
 
 const app = express();
 
+app.get('/', (_, res) => {
+    res.send('Welcome to the Tejodoro app!');
+});
+
 app.get('/api/hello', function (_, res) {
     res.send('Hello World');
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`App is running in ${port}...`)
+app.listen(Number(port), '0.0.0.0', () => {
+    console.log(`App is running in ${port}...`);
 });
