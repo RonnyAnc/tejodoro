ef88b6e34c766d8919f284dea5a7f314119e6dcd
Ronny Ancorini Rodríguez <ronnyancorini@gmail.com>
2022-04-04 19:43:16 +0200

    Add a change to frontend

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Fran and Ronny
         </a>
       </header>
     </div>
