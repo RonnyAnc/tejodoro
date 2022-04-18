94fcef9719b9d421f653b1785c2d101826559926
Fran Ortiz <fortizabril@gmail.com>
2022-04-18 18:38:30 +0200

    WIP: test pomodoro counter

--- webapp/src/App.test.tsx
+++ webapp/src/App.test.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { fireEvent, render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App test", () => {
+    beforeEach(() => {
+        jest.useFakeTimers();
+    });
+
+    test("should allow to start a pomodoro", () => {
+        const { getByTestId, queryByTestId } = render(<App />);
+
+        const startButton = getByTestId("start-pomodoro");
+        fireEvent.click(startButton);
+        expect(queryByTestId("start-pomodoro")).not.toBeInTheDocument();
+        expect(queryByTestId("start-break")).not.toBeInTheDocument();
+        jest.advanceTimersByTime(1000 * 60);
+        jest.advanceTimersByTime(1000 * 60);
+        expect(getByTestId("pomodoro-counter")).toContain("24");
+        jest.runOnlyPendingTimers();
+
+        getByTestId("pomodoro-finished-message");
+    });
+
+    test("should allow to take a break after finishing a pomodoro", () => {
+        const { getByTestId, queryByTestId } = render(<App />);
+
+        const startButton = getByTestId("start-pomodoro");
+        expect(queryByTestId("start-break")).not.toBeInTheDocument();
+        fireEvent.click(startButton);
+        jest.runOnlyPendingTimers();
+
+        const startBreakButton = getByTestId("start-break");
+        fireEvent.click(startBreakButton);
+        jest.runOnlyPendingTimers();
+
+        getByTestId("break-finished-message");
+    });
 });
--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Fran and Ronny
-        </a>
-      </header>
-    </div>
-  );
-}
+type SessionStatus =
+    | "NotStarted"
+    | "PomodoroRunning"
+    | "PomodoroFinished"
+    | "BreakRunning"
+    | "BreakFinished";
+
+const DEFAULT_POMODORO_DURATION = 25;
+const DEFAULT_BREAK_DURATION = 5;
+
+const App: React.FunctionComponent = () => {
+    const [countdownInSeconds, setCountdown] = useState<number>(
+        DEFAULT_POMODORO_DURATION * 60
+    );
+    const [sessionStatus, setSessionStatus] =
+        useState<SessionStatus>("NotStarted");
+
+    useEffect(() => {
+        countdownInSeconds > 0 &&
+            setTimeout(() => {
+                setCountdown(countdownInSeconds - 1);
+            }, 1000);
+    }, [countdownInSeconds]);
+
+    function startPomodoro() {
+        setSessionStatus("PomodoroRunning");
+        const secondsInAMinute = 60;
+        setInterval(() => {
+            setSessionStatus("PomodoroFinished");
+        }, DEFAULT_POMODORO_DURATION * 1000 * secondsInAMinute);
+    }
+
+    function startBreak() {
+        const secondsInAMinute = 60;
+        setSessionStatus("BreakRunning");
+        setInterval(() => {
+            setSessionStatus("BreakFinished");
+        }, DEFAULT_BREAK_DURATION * 1000 * secondsInAMinute);
+    }
+
+    return (
+        <div className="App">
+            {sessionStatus !== "PomodoroRunning" && (
+                <>
+                    <button
+                        data-testid="start-pomodoro"
+                        onClick={startPomodoro}
+                    >
+                        Start pomodoro
+                    </button>
+                </>
+            )}
+            {sessionStatus === "PomodoroRunning" && (
+                <>
+                    <div data-testid="pomodoro-counter">
+                        {countdownInSeconds / 60}
+                    </div>
+                </>
+            )}
+            {sessionStatus === "PomodoroFinished" && (
+                <>
+                    <p data-testid="pomodoro-finished-message">
+                        Pomodoro has finished
+                    </p>
+                    <button data-testid="start-break" onClick={startBreak} />
+                </>
+            )}
+            {sessionStatus === "BreakFinished" && (
+                <p data-testid="break-finished-message">Break has finished</p>
+            )}
+        </div>
+    );
+};
 
 export default App;
