8c53185aa7929286ffe5e1317177fa6172025ead
Fran Ortiz <fortizabril@gmail.com>
2022-07-08 10:36:37 +0200

    Deploy webapp in AWS

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,76 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-
-type SessionStatus =
-    | "NotStarted"
-    | "PomodoroRunning"
-    | "PomodoroFinished"
-    | "BreakRunning"
-    | "BreakFinished";
-
-const DEFAULT_POMODORO_DURATION = 25;
-const DEFAULT_BREAK_DURATION = 5;
+import Countdown from 'react-countdown';
 
 const App: React.FunctionComponent = () => {
-    const [countdownInSeconds, setCountdown] = useState<number>(
-        DEFAULT_POMODORO_DURATION * 60
-    );
-    const [sessionStatus, setSessionStatus] =
-        useState<SessionStatus>("NotStarted");
-
+    const [endTime, setEndtime] = useState<Date>();
+    const [participants, setParticipants] = useState<Array<{ username: string }>>([])
     useEffect(() => {
-        countdownInSeconds > 0 &&
-            setTimeout(() => {
-                setCountdown(countdownInSeconds - 1);
-            }, 1000);
-    }, [countdownInSeconds]);
-
-    function startPomodoro() {
-        setSessionStatus("PomodoroRunning");
-        const secondsInAMinute = 60;
-        setInterval(() => {
-            setSessionStatus("PomodoroFinished");
-        }, DEFAULT_POMODORO_DURATION * 1000 * secondsInAMinute);
-    }
-
-    function startBreak() {
-        const secondsInAMinute = 60;
-        setSessionStatus("BreakRunning");
-        setInterval(() => {
-            setSessionStatus("BreakFinished");
-        }, DEFAULT_BREAK_DURATION * 1000 * secondsInAMinute);
-    }
+        fetch(`${process.env.REACT_APP_TEJODORO_API_URL}`)
+          .then((response) => response.json())
+          .then((session) => {
+            setEndtime(session.status.endTime);
+            setParticipants(session.participants)
+          });
+      }, []);
 
     return (
-        <div className="App">
-            {sessionStatus !== "PomodoroRunning" && (
-                <>
-                    <button
-                        data-testid="start-pomodoro"
-                        onClick={startPomodoro}
-                    >
-                        Start pomodoro
-                    </button>
-                </>
-            )}
-            {sessionStatus === "PomodoroRunning" && (
-                <>
-                    <div data-testid="pomodoro-counter">
-                        {countdownInSeconds / 60}
-                    </div>
-                </>
-            )}
-            {sessionStatus === "PomodoroFinished" && (
-                <>
-                    <p data-testid="pomodoro-finished-message">
-                        Pomodoro has finished
-                    </p>
-                    <button data-testid="start-break" onClick={startBreak} />
-                </>
-            )}
-            {sessionStatus === "BreakFinished" && (
-                <p data-testid="break-finished-message">Break has finished</p>
-            )}
+        <div>
+            {endTime && <Countdown date={endTime} />}
+            <div>
+                Participants:
+                {
+                    participants.map(participant =>
+                        <p>
+                        {participant.username}
+                        </p>
+                    )
+                }
+            </div>
+
         </div>
     );
 };
