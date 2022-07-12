0a8075c75df692f8f53bd853b2c20fb64e1c53da
Fran Ortiz <fortizabril@gmail.com>
2022-07-12 17:18:11 +0200

    Update handler endpoint

--- tejodoro-api/handler.ts
+++ tejodoro-api/handler.ts
@@ -1,28 +1,25 @@
-import { Handler } from 'aws-lambda';
+import { Handler } from "aws-lambda";
 
 export const hello: Handler = (_event: any) => {
-  const now = new Date();
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true,
-    },
-    body: JSON.stringify(
-      {
-        status: { name: 'pomodoro', endTime: now.setMinutes(now.getMinutes() + 25) },
-        participants: [
-          {
-            username: 'fran'
-          },
-          {
-            username: 'ronny'
-          }
-        ],
-      },
-      null,
-      2
-    ),
-  };
-  return new Promise(resolve => resolve(response))
-}
+    const now = new Date();
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true,
+        },
+        body: JSON.stringify(
+            {
+                name: "aPomodoro",
+                status: {
+                    name: "pomodoro",
+                    endTime: now.setMinutes(now.getMinutes() + 25),
+                },
+                participants: ["fran", "ronny"],
+            },
+            null,
+            2
+        ),
+    };
+    return new Promise((resolve) => resolve(response));
+};
