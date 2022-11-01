5dae82ea1b8d818365df38ee054446b81f0cd556
Pablo Rodríguez Guillén <pablorodriguezguillen@gmail.com>
2022-11-01 17:26:40 +0100

    Implement get in DynamoDB repository

--- tejodoro-api/src/infrastructure/SessionDynamoDBRepository.ts
+++ tejodoro-api/src/infrastructure/SessionDynamoDBRepository.ts
@@ -1,12 +1,16 @@
 import { Session } from '../domain/Session';
 import { SessionRepository } from '../domain/SessionRepository';
 import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 export class SessionDynamoDBRepository implements SessionRepository {
   constructor(private client: DynamoDB) {}
 
   async get(name: string): Promise<Session> {
-    throw new Error('Method not implemented.');
+    const { Item: sessionItem } = await this.client.getItem({ TableName: 'Sessions', Key: { name: { S: name } } });
+    const unmarshalledSession = unmarshall(sessionItem!);
+    const retrievedSession = { ...unmarshalledSession, participants: Array.from(unmarshalledSession.participants)};
+    return retrievedSession as Session;
   }
 
   async save(session: Session) {
--- tejodoro-api/test/SessionDynamoDBRepository.test.ts
+++ tejodoro-api/test/SessionDynamoDBRepository.test.ts
@@ -4,24 +4,41 @@ import { DynamoDB } from '@aws-sdk/client-dynamodb';
 import { unmarshall } from '@aws-sdk/util-dynamodb';
 
 describe('SessionDynamoDBRepository', () => {
-  const client = new DynamoDB({ region: 'eu-west-1', endpoint: 'http://localhost:4566', credentials: { accessKeyId: '', secretAccessKey: '' } });
+  const client = new DynamoDB({
+    region: 'eu-west-1',
+    endpoint: 'http://localhost:4566',
+    credentials: { accessKeyId: '', secretAccessKey: '' }
+  });
+
+  const session = {
+    name: 'aSessionName',
+    status: {
+      name: SessionStatus.POMODORO,
+      endTime: Date.now(),
+    },
+    participants: ['fran'],
+  };
+
+  let repository: SessionDynamoDBRepository;
+
+  beforeEach(() => {
+    repository = new SessionDynamoDBRepository(client);
+  })
 
   it('should save a new session', async () => {
-    const session = {
-      name: 'aSessionName',
-      status: {
-        name: SessionStatus.POMODORO,
-        endTime: Date.now(),
-      },
-      participants: ['fran'],
-    };
-    const repository = new SessionDynamoDBRepository(client);
-
     await repository.save(session);
 
     const { Item: sessionItem } = await client.getItem({ TableName: 'Sessions', Key: { name: { S: 'aSessionName' } } });
     const unmarshalledSession = unmarshall(sessionItem!);
     const retrievedSession = { ...unmarshalledSession, participants: Array.from(unmarshalledSession.participants) };
+    expect(retrievedSession).toEqual(session);
+  });
+
+  it('should get the persisted session session', async () => {
+    await repository.save(session);
+
+    const retrievedSession = await repository.get(session.name);
+
     expect(retrievedSession).toEqual(session);
   });
 });
